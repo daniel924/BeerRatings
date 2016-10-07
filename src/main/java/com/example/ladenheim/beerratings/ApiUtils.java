22b2c2ae7b914865c39d1fa9777f516a7e5ab56a
daniel924 <ladenheim@gmail.com>
2016-10-07 00:04:28 -0400

    Add some more scraping logic.

--- src/main/java/com/example/ladenheim/beerratings/ApiUtils.java
+++ src/main/java/com/example/ladenheim/beerratings/ApiUtils.java
@@ -1,7 +1,87 @@
 package com.example.ladenheim.beerratings;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by ladenheim on 10/5/16.
  */
 public class ApiUtils {
+
+    private static final int CONNECTION_TIMEOUT_MS = 15000;
+    private static final int READ_TIMEOUT_MS = 5000;
+    private static String LOG_TAG = ApiUtils.class.getSimpleName();
+    private static String baseUrl = "https://www.beermenus.com";
+
+    public static List<String> FindPlace(String searchVal) throws IOException  {
+        searchVal = "fools gold";
+        String request = baseUrl + "/search?q=" + URLEncoder.encode(searchVal, "UTF-8");
+        String response = "";
+        try {
+            response = getRequest(request);
+        }
+        catch(SocketTimeoutException ex) {
+            Log.d(LOG_TAG, "Response timed out with connection_timeout=%s" + CONNECTION_TIMEOUT_MS +
+                    " and read_timeout=" + READ_TIMEOUT_MS);
+        }
+
+        Pattern p = Pattern.compile("<a data-screen-title=(.*?)>");
+        Matcher m = p.matcher(response);
+        String place_html = "";
+        while(m.find()) {
+            place_html = m.group();
+            break;
+        }
+        String place_url = "";
+        if(!place_html.equals("")) {
+            int start = place_html.indexOf("href=\"") + 7;
+            int end = place_html.indexOf("\">");
+            place_url = place_html.substring(start, end);
+        }
+
+        String placeResponse = getRequest(baseUrl + "/" + place_url);
+
+        p = Pattern.compile("<h3 class=\"mb-0\">(.*?)</h3>");
+        m = p.matcher(placeResponse);
+        List<String> beers = new ArrayList<String>();
+
+        while(m.find()) {
+            beers.add(m.group(1).trim());
+        }
+        return beers;
+    }
+
+    private static String getRequest(String url) throws IOException {
+        URLConnection conn = new URL(url).openConnection();
+        conn.setConnectTimeout(CONNECTION_TIMEOUT_MS);
+        conn.setReadTimeout(READ_TIMEOUT_MS);
+        InputStream inputStream = conn.getInputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while((line = reader.readLine()) != null ) {
+            result.append(line);
+        }
+        return result.toString();
+
+    }
+
+
 }
--- src/main/java/com/example/ladenheim/beerratings/SearchActivity.java
+++ src/main/java/com/example/ladenheim/beerratings/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.ladenheim.beerratings;
 import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -27,70 +29,27 @@ public class SearchActivity extends AppCompatActivity {
     public PlaceAdapter adapter = null;
     // GpsFinder gpsFinder = null;
 
-    /*
-    class SearchTask extends AsyncTask<String, Void, List<Bar>> {
+
+    class SearchTask extends AsyncTask<String, Void, List<String>> {
         ProgressDialog progress = null;
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progress = new ProgressDialog(FindBarActivity.this);
-            progress.setTitle("Searching");
-            progress.setMessage("In Progress");
-            progress.show();
-        }
 
         @Override
-        protected List<Beer> doInBackground(String... params) {
+        protected List<String> doInBackground(String... params) {
+            List<String> response = new ArrayList<String>();
             try {
-                List<Beer> response = null;
-                if (gpsFinder.canGetLocation()) {
-                    double lat = gpsFinder.getLatitude();
-                    double lon = gpsFinder.getLongitude();
-                    response = ApiUtils.FindBarByName(params[0], lat, lon);
-                } else {
-                    response = ApiUtils.FindBarByName(params[0], -1, -1);
-                }
-                final List<Bar> bars = response;
-                if (response == null) {
-                    final String notFound = "Could not find results for: " + params[0];
-                    FindBarActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            listView.setVisibility(View.GONE);
-                            TextView notFoundText = (TextView) findViewById(R.id.not_found_txt);
-                            notFoundText.setVisibility(View.VISIBLE);
-                            notFoundText.setText(notFound);
-                        }
-                    });
-                    progress.dismiss();
-                    return null;
-                }
-                Log.d(LOG_TAG, "Response: " + response.toString());
-                FindBarActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        findViewById(R.id.not_found_txt).setVisibility(View.GONE);
-                        adapter.setList(bars);
-                        listView.setVisibility(View.VISIBLE);
-                    }
-                });
-                return response;
-            } catch (IOException ex) {
+                response = ApiUtils.FindPlace(params[0]);
+            } catch (Exception ex) {
                 Log.d(LOG_TAG, ex.getMessage());
                 ex.printStackTrace();
                 return null;
             }
+            return response;
         }
 
-        @Override
-        protected void onPostExecute(List<Bar> result) {
-            if(progress.isShowing()) {
-                progress.dismiss();
-            }
-        }
+
     }
-    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,7 +61,9 @@ public class SearchActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
 
-        /*
+
+        final SearchView searchView = (SearchView) findViewById(R.id.search_text);
+
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -110,7 +71,7 @@ public class SearchActivity extends AppCompatActivity {
                 new SearchTask().execute(searchView.getQuery().toString());
             }
         });
-        */
+
 
     }
 
